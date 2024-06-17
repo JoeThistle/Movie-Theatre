38b596ffb7d3c9e465e60269e63e6249452a1e9d
JoeThistle <77817646+JoeThistle@users.noreply.github.com>
2024-06-18 11:10:23 +1200

    Now with time buttons
    Allows the user to select a time, I will next work on allowing them to open a ticket booking window.

--- Movie Theatre.py
+++ Movie Theatre.py
@@ -1,53 +1,66 @@
 #Title: Movie Theatre
 #Author: Joe Thistlethwaite
 #Purpose: To allow for users to book tickets for a movie
-#Version: 1.0
+#Version: 1.5
 from tkinter import *
 from tkinter import ttk
 import random
+import time
+
 MAX_SEATS = 150
-movies = {"Kingsmen":6,"Oppenheimer":3,"The Italian Job":4,"Doctor Who":3,"Five Nights At Freddy's":3,"Despicable Me 4":5}
-times = ["10:00am","10:10am","12:30pm","1:40pm","2:00pm","2:30pm","4:00pm","4:40pm","6:10pm","8:00pm"]
+movies = {"Kingsmen": 6,"Oppenheimer": 3,"The Italian Job": 4,"Doctor Who": 3,"Five Nights At Freddy's": 3,"Despicable Me 4": 5}
+times = ["10:00am", "10:10am", "12:30pm", "1:40pm", "2:00pm", "2:30pm", "4:00pm", "4:40pm", "6:10pm", "8:00pm"]
 
 class Movie:
-    def __init__(self,times,seats=MAX_SEATS,*args):
+    def __init__(self, master, movie_name, times):
         '''This function initialises all the variables'''
-        self.movie = drop_box.get()
+        self.master = master
+        self.movie_name = movie_name
         self.times = times
-        self.seats = seats
-        self.create_movie_window()
-    
-    def create_movie_window(self):
-        '''Creates the window for ticket selection based on movie choice'''
-        self.movie_window = Tk()
-        self.movie_window.title(self.movie)
-        for i in range(len(self.times)):
-            print(i)
-        self.movie_window.mainloop()
+        self.create_movie_times()
 
+    def create_movie_times(self):
+        '''Creates the times for ticket selection based on movie choice'''
+        btn_clear()
+        for num, time in enumerate(self.times):
+            time_btn = Button(self.master, text=time, command=())
+            time_btn.grid(padx=10, pady=10, column=2, row=(num+2), sticky="WE")
 
-def dropdown(movie_list):
+def main_menu(movie_list):
     '''This function creates the dropdown menu based off of what movies there are'''
     global drop_box
     global movie_selected
     movie_selected = StringVar()
-    drop_box = ttk.Combobox(window,textvariable=movie_selected,state="readonly")
+    drop_box = ttk.Combobox(window, textvariable=movie_selected, state="readonly")
     drop_box['values'] = movie_list
-    drop_box.grid(padx=10,pady=10,row=2,column=0)
+    drop_box.grid(padx=10, pady=10, row=2, column=0,sticky="WE")
+    movie_lbl = Label(window, text="Movie:")
+    time_lbl = Label(window, text="Times:")
+    movie_lbl.grid(row=1, column=0, sticky="WE")
+    time_lbl.grid(row=1, column=2, sticky="WE")
 
 def choose_times(*args):
     '''Selects the time slots for the movie'''
-    print("test")
-    usable_times = times
-    time_slots = []
-    for i in range(movies[movie_selected.get()]):
-        time_slots.append(random.choice(usable_times))
-        usable_times.remove(time_slots[-1])
-    Movie(time_slots)
+    chosen_times = random.sample(times, movies[movie_selected.get()])
+    #The following three lines were used from the following link: https://stackoverflow.com/questions/40187525/how-to-sort-a-list-of-times
+    #Format to convert to a time object
+    time_format = '%I:%M%p'
+    #Creates list and converts them using times chosen
+    time_hours = [time.strptime(t, time_format) for t in chosen_times]
+    #Sorts the list using the new objects
+    chosen_times = [time.strftime(time_format, h) for h in sorted(time_hours)]
+    Movie(window, movie_selected.get(), chosen_times)
+    global prev
+    prev = movie_selected.get()
 
-#Main Program
+def btn_clear():
+    clear = Frame(window)
+    clear.grid(padx=10, pady=10,row=2,column=2,rowspan=6,sticky="NESW")
+
+# Main Program
 window = Tk()
 window.title("Movie Theatre")
-dropdown(list(movies.keys()))
-movie_selected.trace_add("write",choose_times)
+window.geometry(newGeometry="250x300")
+main_menu(list(movies.keys()))
+movie_selected.trace_add("write", choose_times)
 window.mainloop()
