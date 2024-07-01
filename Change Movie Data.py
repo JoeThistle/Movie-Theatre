8b8332b69b81e4aa8fe8eb6d760b573f2e050d86
JoeThistle <77817646+JoeThistle@users.noreply.github.com>
2024-07-02 11:07:28 +1200

    Allows for changing of movie showing numbers
    This allows for users to change the number of showings a movie has and has a few bug fixes to improve usability.

--- Change Movie Data.py
+++ Change Movie Data.py
@@ -1,7 +1,7 @@
 #Title: Change Movie Data
 #Author: Joe Thistlethwaite
 #Purpose: To allow for staff to change movie data
-#Version: 1.0
+#Version: 1.4
 
 from tkinter import *
 from tkinter import ttk
@@ -10,7 +10,65 @@ import json
 with open("Movie Data.json") as file:
     data = json.loads(file.read())
 
+def showings():
+    ''''''
+    global movie_selected
+    global movies
+    movies = data['movies&showings']
+    movie_selected = StringVar()
+    movie_drop_box = ttk.Combobox(window, textvariable=movie_selected, state="readonly")
+    movie_drop_box['values'] = list(movies.keys())
+    movie_drop_box.grid(row=2,column=0)
+    movie_drop_box.bind("<<ComboboxSelected>>", show_showings)
+
+
+def show_showings(*args):
+    global showings_ent
+    try:
+        showings_ent.destroy()
+    except:
+        pass
+    num_of_showings = movies[movie_selected.get()]
+    showings_ent = Entry(window)
+    #I aquired the following line from https://www.geeksforgeeks.org/how-to-set-the-default-text-of-tkinter-entry-widget/
+    showings_ent.insert(0, num_of_showings)
+    showings_ent.grid(row=2, column=1)
+    save_btn = Button(window,text="Save",command=save_showings)
+    time_btn.grid_forget()
+    save_btn.grid(row=1,column=1)
+
+def save_showings():
+    current_movie = movie_selected.get()
+    global show_error_lbl
+    try:
+        current_showings = int(showings_ent.get())
+        if current_showings >= 0:
+            movies[current_movie] = current_showings
+            with open("Movie Data.json","w") as outfile:
+                new_data = json.dumps(data,indent=2)
+                outfile.write(new_data)
+        else:
+            try:
+                show_error_lbl.grid_remove()
+            except:
+                pass
+            show_error_lbl = Label(window,text="Please Enter A\nPossible Number")
+            show_error_lbl.grid(row=3,column=1)
+    except:
+        try:
+                show_error_lbl.grid_remove()
+        except:
+            pass
+        show_error_lbl = Label(window,text="Please Enter A Number")
+        show_error_lbl.grid(row=3,column=1)
+
 window = Tk()
+main_lbl = Label(window,text="What do you wish to change?")
+showings_btn = Button(window,text="Showings",command=showings)
+time_btn = Button(window,text="Time Slots",command=())
+main_lbl.grid(row=0,column=0,columnspan=2)
+time_btn.grid(row=1,column=1)
+showings_btn.grid(row=1,column=0)
 window.title("Movie Data")
-window.geometry("250x275")
+window.geometry("300x325")
 window.mainloop()
